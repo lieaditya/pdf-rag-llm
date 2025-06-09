f43a012a3ec1803cb8dc8d55bb33cf0e15345f26
lieaditya <adityabryan.lie@gmail.com>
2025-06-09 23:12:32 +0200

    chore: use polling for wsl auto reload

--- rag-frontend/next.config.ts
+++ rag-frontend/next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import type { webpack } from 'next/dist/compiled/webpack/webpack'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  // use polling for WSL
+  webpackDevMiddleware: (config: webpack.Configuration) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
